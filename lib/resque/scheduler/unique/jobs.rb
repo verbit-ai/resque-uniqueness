0bc13c439d7e8746e7ab0285e469b5439e4aa4ca
Olexandr Hoshylyk <gashuk95@gmail.com>
2019-07-08 14:33:47 +0300

    Init app

---
+++ lib/resque/scheduler/unique/jobs.rb
@@ -0,0 +1,12 @@
+require "resque/scheduler/unique/jobs/version"
+
+module Resque
+  module Scheduler
+    module Unique
+      module Jobs
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/resque/scheduler/unique/jobs/version.rb
@@ -0,0 +1,9 @@
+module Resque
+  module Scheduler
+    module Unique
+      module Jobs
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
