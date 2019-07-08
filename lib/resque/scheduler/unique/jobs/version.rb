e0c58c2dc873ed02c65a70868f787b9097d909b3
Olexandr Hoshylyk <gashuk95@gmail.com>
2019-07-08 14:39:45 +0300

    Fix gemspec

--- lib/resque/scheduler/unique/jobs/version.rb
+++ lib/resque/scheduler/unique/jobs/version.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 module Resque
   module Scheduler
     module Unique
       module Jobs
-        VERSION = "0.1.0"
+        VERSION = '0.1.0'
       end
     end
   end
