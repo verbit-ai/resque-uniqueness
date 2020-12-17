d13f481bb2f90127c7958a064a9b40d4263ca3c9
Olexandr Hoshylyk <gashuk95@gmail.com>
2020-12-17 12:40:59 +0200

    HOTFIX: improve performing lock

--- spec/acceptance/resque/plugins/uniqueness/jobs_extractor_spec.rb
+++ spec/acceptance/resque/plugins/uniqueness/jobs_extractor_spec.rb
@@ -3,16 +3,17 @@
 RSpec.describe Resque::Plugins::Uniqueness::JobsExtractor, type: :acceptance do
   # In cases when jobs moves from scheduled to queued the system could think that this job has
   # unreleased lock. But it's not. So, this spec ensure that it will not happen.
-  describe '.with_unreleased_queueing_lock' do
+  describe 'Check unreleased locks' do
     before do
       500.times {
         Resque.enqueue_in(rand(1..15), JobsExtractorAcceptanceWorker, SecureRandom.uuid)
       }
     end
 
-    it('is not have unreleased locks', :aggregate_failures) do
+    it('is not have unreleased locks', :aggregate_failures) do # rubocop:disable RSpec/ExampleLength
       workers_waiter do
         expect(described_class.with_unreleased_queueing_lock(10)).to eq []
+        expect(described_class.with_unreleased_performing_lock).to eq []
       end
 
       expect(described_class.with_unreleased_queueing_lock(10)).to eq []
